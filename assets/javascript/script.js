fc4161eb39d107c49453788d9be3935c521b872e
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-06-26 23:21:10 -0400

    integrated styling. completed nav styling. incorporated search tools.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,4 +4,36 @@ searchButton.addEventListener("click", function(){
     console.log(textInput.value)
 } )
 
+$( function() {
+    var dateFormat = "mm/dd/yy",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+ 
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+ 
+      return date;
+    }
+  } ); 
 
+  
