f83918a8cf7a52e535ec7a03ff7a2854088edf3c
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-07-03 23:04:44 -0400

    fixed all bugs with images, loads, search, etc. FOC.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,19 +30,11 @@ $( function() {
   }
 } ); 
 
-
+var doc;
 
 function defaultNews() {
-  // if(isFetching) return;
-  // isFetching = true;
-
-  // Select the dropdown element
-  var dropdown = document.getElementById("news-categories");
-
-  // Select the element where you want to display the selected option
-  var categoryElement = document.getElementById("h2-category");
-
   // Function to update the display for top news
+  var categoryElement = document.getElementById("h2-category");
   function updateDisplay() {
     categoryElement.textContent = 'Top News This Week'; // Update the display element with the selected option
   }
@@ -60,137 +52,11 @@ function defaultNews() {
 
       for (let n=0; n<20; n++) {
         doc = data.results[n];
-
-        // Process the received data here
-        // Access the snippet
-        const snippet = doc.abstract;
-        console.log('Snippet:', snippet);
-
-        // Access the main headline
-        const mainHeadline = doc.title;
-        console.log('Main Headline:', mainHeadline);
-
-        // Access the source
-        const source = doc.source;
-        console.log('Source:', source);
-
-        // Access the date
-        const date = doc.published_date;
-        console.log('Date:', date);
-
-        // Access the date
-        const category = doc.section;
-        console.log('Category:', category);
-
-        // Access the author
-        const author = doc.byline;
-        console.log('Author:', author);
-
-        // Create the news widget to feed api responses into  
-        function createNewsWidget() {
-          // Create the main section element
-          const newsWidgetSection = document.getElementById('news-results');
-
-          // Create the news widget container
-          const newsWidgetContainer = document.createElement('section');
-          newsWidgetContainer.id = 'news-widget';
-
-          // Create the widget content
-          const widgetContent = document.createElement('div');
-          widgetContent.id = 'widget-content';
-
-          // Create the widget header
-          const widgetHeader = document.createElement('div');
-          widgetHeader.id = 'widget-header';
-
-          // Create the news source element
-          const newsSource = document.createElement('div');
-          newsSource.id = 'news-source';
-          newsSource.textContent = source;
-
-          // Create the article date element
-          const articleDate = document.createElement('div');
-          articleDate.id = 'article-date';
-          displayDate = date.match(/^\d{4}-\d{2}-\d{2}/)[0];
-          console.log(displayDate); // Output: 2023-06-27
-          articleDate.textContent = displayDate;
-
-          // Append news source and article date to widget header
-          widgetHeader.appendChild(newsSource);
-          widgetHeader.appendChild(articleDate);
-
-          // Create article title
-          const articleTitle = document.createElement('div');
-          articleTitle.id = 'article-title';
-          articleTitle.textContent = mainHeadline;
-
-          // Create article summary
-          const articleSummary = document.createElement('div');
-          articleSummary.id = 'article-summary';
-          articleSummary.textContent = snippet;
-
-          // Create author and category section
-          const authorCategory = document.createElement('div');
-          authorCategory.id = 'author-category';
-
-          // Create author element within author and category section
-          const authorName = document.createElement('div');
-          authorName.id = 'author';
-          authorName.innerHTML = '<b>Author:</b> ' + author.replace(/^By /, '');
-
-          // Create category element within author and category section
-          const categoryName = document.createElement('div');
-          categoryName.id = 'category';
-          categoryName.innerHTML = '<b>Category:</b> ' + category;
-
-          // Append author and category to author and category section
-          authorCategory.appendChild(authorName);
-          authorCategory.appendChild(categoryName);
-
-          // Append all elements to widget content
-          widgetContent.appendChild(widgetHeader);
-          widgetContent.appendChild(articleTitle);
-          widgetContent.appendChild(articleSummary);
-          widgetContent.appendChild(authorCategory);
-
-          // Create the article image
-          const articleImage = document.createElement('div');
-          articleImage.id = 'article-image';
-
-          // Create the image element within the article image
-          // Use a try block - the multimedia might not be an array or the specific index might not be available
-          let thumbnailUrl;
-          try {
-            thumbnailUrl = data.results[n].media[0].media-metadata[0].url;
-            console.log("Thumbnail: " + thumbnailUrl);
-          } catch (e) {
-            console.log('Failed to access thumbnail URL:', e);
-            thumbnailUrl = './assets/images/monkey-selfie.jpg'; // default thumbnail URL or leave it undefined
-          } 
-          const image = document.createElement('img');
-          image.src = thumbnailUrl;
-          image.alt = 'Missing Image';
-          console.log(thumbnailUrl)
-
-          // Append image to article image
-          articleImage.appendChild(image);
-
-          // Append widget content and article image to the news widget container
-          newsWidgetContainer.appendChild(widgetContent);
-          newsWidgetContainer.appendChild(articleImage);
-
-          // Append the news widget container to the main section element
-          newsWidgetSection.appendChild(newsWidgetContainer);
-        }
-
-        // Call the function to create the news widget
-        createNewsWidget();
+        populateArticles();
         updateDisplay();
       }
-      // isFetching = false;
     })
     .catch(error => console.log(error));
-    // isFetching = false;  
 }
 
 // Call searchNews() on page load with the default URL
@@ -198,8 +64,174 @@ $(document).ready(function() {
   defaultNews();
 });
 
+function populateArticles() {
+  // Access the snippet
+  let summary;
+  if (doc.abstract) {
+    summary = doc.abstract;
+  } else {
+    summary = doc.snippet;
+  }
+  console.log('Summary:', summary.substring(0,50));
 
+  // Access the main headline
+  let title;
+  if (doc.title) {
+    title = doc.title;
+  } else {
+    title = doc.headline.main;
+  }
+  console.log('Title:', title.substring(0,50));
 
+  // Access the source
+  let source;
+  source = doc.source;
+  console.log('Source:', source);
+
+  // Access the date
+  let date;
+  if (doc.published_date) {
+    date = doc.published_date;
+  } else {
+    date = doc.pub_date;
+  }
+  console.log('Date:', date);
+  console.log('dayjs Date:', dayjs(date).format('YYYYMMDD'))
+
+  // Access the date
+  let category;
+  if (doc.section) {
+    category = doc.section
+  } else { 
+    category = doc.section_name;
+  }
+  console.log('Category:', category);
+
+  // Access the author
+  let author;
+  author = doc.byline && doc.byline.original ? doc.byline.original.replace(/^By /, '') : 'N/A';
+  console.log('Author:', author);
+
+  // Create the news widget to feed api responses into  
+  function createNewsWidget() {
+    // Create the main section element
+    const newsWidgetSection = document.getElementById('news-results');
+
+    // Create the news widget container
+    const newsWidgetContainer = document.createElement('section');
+    newsWidgetContainer.id = 'news-widget';
+
+    // Create the widget content
+    const widgetContent = document.createElement('div');
+    widgetContent.id = 'widget-content';
+
+    // Create the widget header
+    const widgetHeader = document.createElement('div');
+    widgetHeader.id = 'widget-header';
+
+    // Create the news source element
+    const newsSource = document.createElement('div');
+    newsSource.id = 'news-source';
+    newsSource.textContent = source;
+
+    // Create the article date element
+    const articleDate = document.createElement('div');
+    articleDate.id = 'article-date';
+    displayDate = date.match(/^\d{4}-\d{2}-\d{2}/)[0];
+    console.log(displayDate); // Output: 2023-06-27
+    articleDate.textContent = displayDate;
+
+    // Append news source and article date to widget header
+    widgetHeader.appendChild(newsSource);
+    widgetHeader.appendChild(articleDate);
+
+    // Create article title
+    const articleTitle = document.createElement('div');
+    articleTitle.id = 'article-title';
+    articleTitle.textContent = title;
+
+    // Create article summary
+    const articleSummary = document.createElement('div');
+    articleSummary.id = 'article-summary';
+    articleSummary.textContent = summary;
+
+    // Create author and category section
+    const authorCategory = document.createElement('div');
+    authorCategory.id = 'author-category';
+
+    // Create author element within author and category section
+    const authorName = document.createElement('div');
+    authorName.id = 'author';
+    authorName.innerHTML = '<b>Author:</b> ' + author.replace(/^By /, '');
+
+    // Create category element within author and category section
+    const categoryName = document.createElement('div');
+    categoryName.id = 'category';
+    categoryName.innerHTML = '<b>Category:</b> ' + category;
+
+    // Append author and category to author and category section
+    authorCategory.appendChild(authorName);
+    authorCategory.appendChild(categoryName);
+
+    // Append all elements to widget content
+    widgetContent.appendChild(widgetHeader);
+    widgetContent.appendChild(articleTitle);
+    widgetContent.appendChild(articleSummary);
+    widgetContent.appendChild(authorCategory);
+
+    // Create the article image
+    const articleImage = document.createElement('div');
+    articleImage.id = 'article-image';
+
+    // Create the image element within the article image
+    // let thumbnailUrl;
+    // try {
+    //   thumbnailUrl = doc.media[0]['media-metadata'][0].url;
+    //   console.log("Thumbnail: " + thumbnailUrl);
+    // } catch (e) {
+    //   console.log('Failed to access thumbnail URL:', e);
+    //   thumbnailUrl = './assets/images/monkey-selfie.jpg'; // default thumbnail URL or leave it undefined
+    // } 
+    // const image = document.createElement('img');
+    // image.src = thumbnailUrl;
+    // image.alt = 'Missing Image';
+    // console.log(thumbnailUrl);
+
+    let thumbnailUrl;
+    try {
+        thumbnailUrl = doc.media[0]['media-metadata'][2].url;
+    } catch (e) {
+        console.log('Failed to access thumbnail URL from media:', e);
+        thumbnailUrl = undefined;  // setting it to undefined for the next try block
+    }
+    if (!thumbnailUrl) {
+        try {
+            thumbnailUrl = 'https://www.nytimes.com/'+doc.multimedia[19].url;
+        } catch (e) {
+            console.log('Failed to access thumbnail URL from multimedia:', e);
+            thumbnailUrl = './assets/images/monkey-selfie.jpg'; // default thumbnail URL
+        }
+    }
+
+    const image = document.createElement('img');
+    image.src = thumbnailUrl;
+    image.alt = 'Missing Image';
+    console.log(thumbnailUrl)
+
+    // Append image to article image
+    articleImage.appendChild(image);
+
+    // Append widget content and article image to the news widget container
+    newsWidgetContainer.appendChild(widgetContent);
+    newsWidgetContainer.appendChild(articleImage);
+
+    // Append the news widget container to the main section element
+    newsWidgetSection.appendChild(newsWidgetContainer);
+  }
+  createNewsWidget()
+}
+
+// Search news using search filters
 function searchNews() {
 
   // Clear the news results container at the start of each search
@@ -208,13 +240,9 @@ function searchNews() {
       newsWidgetSection.firstChild.remove();
   }
 
-  // Select the dropdown element
-  var dropdown = document.getElementById("news-categories");
-
-  // Select the element where you want to display the selected option
-  var categoryElement = document.getElementById("h2-category");
-
   // Function to update the display based on the selected option
+  var dropdown = document.getElementById("news-categories");
+  var categoryElement = document.getElementById("h2-category");
   function updateDisplay() {
     var selectedOption = dropdown.value; // Get the selected option's value
     categoryElement.textContent = selectedOption; // Update the display element with the selected option
@@ -252,8 +280,6 @@ function searchNews() {
   console.log(from);
   console.log(to);
 
-
-
   // Make an API request with the search term
   let apiKey = '0qKub7V4fhsw2VnnCiKqY4UL7fbeJPsg';
 
@@ -267,140 +293,13 @@ function searchNews() {
       console.log(data);
 
       for (let n=0; n<10; n++) {
-        var doc = data.response.docs[n];
-
-        // Process the received data here
-        // Access the snippet
-        const snippet = doc.snippet;
-        console.log('Snippet:', snippet);
-
-        // Access the main headline
-        const mainHeadline = doc.headline.main;
-        console.log('Main Headline:', mainHeadline);
-
-        // Access the source
-        const source = doc.source;
-        console.log('Source:', source);
-
-        // Access the date
-        const date = doc.pub_date;
-        console.log('Date:', date);
-
-        // Access the date
-        const category = doc.section_name;
-        console.log('Category:', category);
-
-        // Access the author
-        // const author = doc.byline.original;
-        const author = doc.byline && doc.byline.original ? doc.byline.original.replace(/^By /, '') : 'N/A';
-        console.log('Author:', author);
-
-        // Create the news widget to feed api responses into  
-        function createNewsWidget() {
-          // Create the main section element
-          const newsWidgetSection = document.getElementById('news-results');
-
-          // Create the news widget container
-          const newsWidgetContainer = document.createElement('section');
-          newsWidgetContainer.id = 'news-widget';
-
-          // Create the widget content
-          const widgetContent = document.createElement('div');
-          widgetContent.id = 'widget-content';
-
-          // Create the widget header
-          const widgetHeader = document.createElement('div');
-          widgetHeader.id = 'widget-header';
-
-          // Create the news source element
-          const newsSource = document.createElement('div');
-          newsSource.id = 'news-source';
-          newsSource.textContent = source;
-
-          // Create the article date element
-          const articleDate = document.createElement('div');
-          articleDate.id = 'article-date';
-          displayDate = date.match(/^\d{4}-\d{2}-\d{2}/)[0];
-          console.log(displayDate); // Output: 2023-06-27
-          articleDate.textContent = displayDate;
-
-          // Append news source and article date to widget header
-          widgetHeader.appendChild(newsSource);
-          widgetHeader.appendChild(articleDate);
-
-          // Create article title
-          const articleTitle = document.createElement('div');
-          articleTitle.id = 'article-title';
-          articleTitle.textContent = mainHeadline;
-
-          // Create article summary
-          const articleSummary = document.createElement('div');
-          articleSummary.id = 'article-summary';
-          articleSummary.textContent = snippet;
-
-          // Create author and category section
-          const authorCategory = document.createElement('div');
-          authorCategory.id = 'author-category';
-
-          // Create author element within author and category section
-          const authorName = document.createElement('div');
-          authorName.id = 'author';
-          authorName.innerHTML = '<b>Author:</b> ' + author.replace(/^By /, '');
-
-          // Create category element within author and category section
-          const categoryName = document.createElement('div');
-          categoryName.id = 'category';
-          categoryName.innerHTML = '<b>Category:</b> ' + category;
-
-          // Append author and category to author and category section
-          authorCategory.appendChild(authorName);
-          authorCategory.appendChild(categoryName);
-
-          // Append all elements to widget content
-          widgetContent.appendChild(widgetHeader);
-          widgetContent.appendChild(articleTitle);
-          widgetContent.appendChild(articleSummary);
-          widgetContent.appendChild(authorCategory);
-
-          // Create the article image
-          const articleImage = document.createElement('div');
-          articleImage.id = 'article-image';
-
-          // Create the image element within the article image
-          // Use a try block - the multimedia might not be an array or the specific index might not be available
-          let thumbnailLgUrl;
-          try {
-            const thumbnailLgLoc = doc.multimedia[19].url;
-            thumbnailLgUrl = 'https://www.nytimes.com/'+thumbnailLgLoc;
-          } catch (e) {
-            console.log('Failed to access thumbnail URL:', e);
-            thumbnailLgUrl = './assets/images/monkey-selfie.jpg'; // default thumbnail URL or leave it undefined
-          } 
-          const image = document.createElement('img');
-          image.src = thumbnailLgUrl;
-          image.alt = 'Article Image';
-
-          // Append image to article image
-          articleImage.appendChild(image);
-
-          // Append widget content and article image to the news widget container
-          newsWidgetContainer.appendChild(widgetContent);
-          newsWidgetContainer.appendChild(articleImage);
-
-          // Append the news widget container to the main section element
-          newsWidgetSection.appendChild(newsWidgetContainer);
-        }
-
-        // Call the function to create the news widget
-        createNewsWidget();
+        doc = data.response.docs[n];
+        populateArticles()
         updateDisplay();
-      
       }
     })
   .catch(error => console.log(error));  
 }
-
-
 
 // hovering over links shows actual link url
 let news_source = $(document.getElementById("our-team-container")).find($("[id=news-source]"));
